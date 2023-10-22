89ae4ebbadea6c7ec4c025a3c1fe9d80884e90d7
Pololops <paul.rigaudeau@me.com>
2023-10-22 10:51:23 +0200

    ci - adapt jest config to typescript and install ts-node

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
  
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
-const config = {
+const config: import('jest').Config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
  
